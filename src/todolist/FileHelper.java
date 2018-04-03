84d371128a5bbfbb238731a4612d4c0567d9ef9f
retrosnob <jusrobertson@yahoo.co.uk@>
2018-04-03 10:11:38 +0800

    Began work on file storage of tasks

--- src/todolist/FileHelper.java
+++ src/todolist/FileHelper.java
@@ -5,10 +5,60 @@
  */
 package todolist;
 
-/**
- *
- * @author justin
- */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class FileHelper {
+
+    // This class helps us to read and write stuff
+    // from/to the disk.
+    final String FILE_NAME = "tasks.sdv";
+    BufferedReader reader;
+    PrintWriter writer;
+
+    public FileHelper() {
+
+    }
     
+    public static void main(String[] args) {
+        System.out.println("Testing my file helper class...");
+        FileHelper f = new FileHelper();
+        String[] lines = f.readTasks();
+        System.out.println(lines[0]);
+        System.out.println(lines[1]);
+        System.out.println(lines[2]);
+    }
+
+    String[] readTasks() {
+        String[] tasks = new String[100];
+        int count = 0;
+        try {
+            reader = new BufferedReader(new FileReader(FILE_NAME));
+            String line = reader.readLine();
+            while (line != null) {
+                tasks[count] = line;
+                count = count + 1;
+                line = reader.readLine();
+            }
+        } catch (FileNotFoundException f) {
+            System.out.print("Tasks file not found");
+        } catch (IOException i) {
+            System.out.print("Cannot read from tasks file");
+
+        }
+        return tasks;
+    }
+
+    void writeTasks() {
+        try {
+            writer = new PrintWriter(new FileWriter(FILE_NAME));
+        } catch (IOException i) {
+            System.out.print("Cannot open tasks file for output");
+        }
+    }
+
 }
